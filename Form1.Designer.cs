dc100733e308ae51932731cf7dd9fbd99af6c910
AlexBlack144 <aboneofficial@gmail.com>
2023-05-07 22:50:25 +0300

    Creat Project
    Music Player (C#, WinForm, Processes)

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace MusicPlayerListBoxTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(384, 248);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            this.axWindowsMediaPlayer1.CurrentItemChange += new AxWMPLib._WMPOCXEvents_CurrentItemChangeEventHandler(this.axWindowsMediaPlayer1_CurrentItemChange);
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(384, 183);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(206, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 61);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Songs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 376);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(384, 183);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(0, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 61);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear List Songs";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 559);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace MusicPlayerListBoxTask
+{
+    public partial class Form1 : Form
+    {
+        List<MediaFile> mediaFiles = new List<MediaFile>();
+        public Form1()
+        {
+            InitializeComponent();
+            var myPlayList = axWindowsMediaPlayer1.playlistCollection.newPlaylist("MyPlayList");
+            axWindowsMediaPlayer1.currentPlaylist = myPlayList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.ValueMember = "Path";
+            listBox1.DisplayMember = "FileName";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = 0;
+            for (int i = 0; i < axWindowsMediaPlayer1.currentPlaylist.count; i++)
+            {
+
+                if (axWindowsMediaPlayer1.currentPlaylist.Item[i].name.Equals(listBox1.SelectedItem.ToString()))
+                {
+                    index = i;
+                    break;
+                    
+                }
+            }
+            axWindowsMediaPlayer1.Ctlcontrols.playItem(axWindowsMediaPlayer1.currentPlaylist.Item[index]);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = true,ValidateNames= true,Filter = "MP3|*.mp3|MP4|*mp4|WAV|*wav"})
+            {
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if(!mediaFiles.Any((x) => x.FileName.Equals(Path.GetFileNameWithoutExtension(openFileDialog.FileName))))
+                    {
+                        listBox1.Items.Add(Path.GetFileNameWithoutExtension(openFileDialog.FileName));
+                        mediaFiles.Add(new MediaFile() { FileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName), Path = openFileDialog.FileName });
+                        var mediaItem = axWindowsMediaPlayer1.newMedia(openFileDialog.FileName);
+                        axWindowsMediaPlayer1.currentPlaylist.appendItem(mediaItem);
+                        axWindowsMediaPlayer1.Ctlcontrols.play();
+                    }
+                } 
+            }
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            this.mediaFiles.Clear();
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsMediaEnded)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.next();
+                //axWindowsMediaPlayer1.Ctlcontrols.;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+
+                //try
+                //{
+
+                //    MediaFile media = listBox1.SelectedItem as MediaFile;
+                //    var task = Task.Factory.StartNew(() =>
+                //    {
+                //        if (media != null)
+                //        {
+                //            axWindowsMediaPlayer1.URL = media.Path;
+                //            axWindowsMediaPlayer1.Ctlcontrols.stop();
+                //            axWindowsMediaPlayer1.Ctlcontrols.play();
+                //        }
+                //    });
+                //    task.Wait();
+
+                //}
+                //catch (Exception ex)
+                //{
+
+                //}
+
+            }
+        }
+
+        private void axWindowsMediaPlayer1_CurrentItemChange(object sender, AxWMPLib._WMPOCXEvents_CurrentItemChangeEvent e)
+        {
+
+            //if(listBox1.Items.Count != 0)
+            //{
+
+            //    listBox1.SelectedIndex = axWindowsMediaPlayer1.Ctlcontrols.currentItem.attributeCount-1;
+            //        //(e.pdispMedia as IWMPMedia).attributeCount - 1;
+            //}
+
+            if (listBox1 == null || listBox1.Items.Count == 0)
+            {
+                return;
+            }
+
+            for (int i = 0; i < axWindowsMediaPlayer1.currentPlaylist.count; i++)
+            {
+
+                if (axWindowsMediaPlayer1.currentPlaylist.Item[i].name.Equals(axWindowsMediaPlayer1.Ctlcontrols.currentItem.name))
+                {
+                    listBox1.SelectedIndex = i;
+
+                    break;
+
+                }
+            }
+        }
+    }
+}
